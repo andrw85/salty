fe95647612307486be4392d357b0715db7a929b0
Andrew O'Doherty <andrew.odoherty@daimler.com>
2021-09-20 18:08:37 +0200

    WIP: refactoring storage. Created vault module

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod options;
 pub mod password;
 pub mod hasher;
 pub mod storage;
+pub mod account;
 pub use options::options;
 pub use options::{Opt, ManagerOpt,PasswordGenOpt, AddSiteOpt};
 pub use storage::{Account, AccountEntry};
