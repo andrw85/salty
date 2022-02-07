da7831c888ff726671f186ed30259a4af36bd1fa
Andrew O'Doherty <andrew.olv@gmail.com>
2022-02-07 19:30:49 +0100

    fix: format code with fmt

--- src/bin/cli_client/vault_cli.rs
+++ src/bin/cli_client/vault_cli.rs
@@ -1,24 +1,26 @@
+use super::options::AddSiteOpt;
+use question::{Answer, Question};
 use salty::utils::*;
-use salty::vault::{vault::{Vault}, account::{AccountEntry}};
-use super::options::{AddSiteOpt};
-use question::{Answer,Question};
+use salty::vault::{account::AccountEntry, vault::Vault};
 
 pub fn add_entry(opt: AddSiteOpt) -> Result<(), String> {
-    let AddSiteOpt{site, user,hasher_salt} = opt;
+    let AddSiteOpt {
+        site,
+        user,
+        hasher_salt,
+    } = opt;
 
     if !Vault::exists() {
-        return Err("No vault, you need to create one first!".to_string())
+        return Err("No vault, you need to create one first!".to_string());
     }
     let password = PasswordQuery::new("Insert Vault master password: ").read();
     let pw_hashed = hasher::hash(&password, &hasher_salt).unwrap();
-    
+
     let mut vault = match Vault::default(&pw_hashed) {
-        Ok(acc) => {
-            acc
-        },
+        Ok(acc) => acc,
         Err(cocoon::Error::Cryptography) => {
             return Err("Invalid vault master password!".to_string());
-        },
+        }
         _ => {
             return Err("Unknown error adding an entry.".to_string());
         }
@@ -29,24 +31,27 @@ pub fn add_entry(opt: AddSiteOpt) -> Result<(), String> {
     let entry = AccountEntry::new(&site, &user, &password);
     if let Err(entry) = vault.account.add(entry) {
         let answer = Question::new("Site already exists, do you want to overwrite it's settings?")
-                    .default(Answer::NO)
-                    .show_defaults()
-                    .confirm();
-    
+            .default(Answer::NO)
+            .show_defaults()
+            .confirm();
+
         if answer == Answer::YES {
             vault.account.force_add(entry);
-        } 
+        }
     }
     // println!("{:#?}",account);
     Ok(())
 }
 
 pub fn create_vault() -> Result<(), String> {
-    let result = PasswordQuery::new("Insert Vault master password").read_and().prompt("Insert one more time: ").confirm_read();
-    
+    let result = PasswordQuery::new("Insert Vault master password")
+        .read_and()
+        .prompt("Insert one more time: ")
+        .confirm_read();
+
     if let Ok(password) = result {
         let pw_hashed = hasher::hash(&password, "aot/I3YepRSH5AaZe+oDEQ").unwrap(); //TODO: make salt configurable
-    
+
         password::check_pass_strength(password)?;
         Vault::new(&pw_hashed);
     }
@@ -55,19 +60,17 @@ pub fn create_vault() -> Result<(), String> {
 
 pub fn show_entries() -> Result<(), String> {
     if !Vault::exists() {
-        return Err("No vault, you need to create one first!".to_string())
+        return Err("No vault, you need to create one first!".to_string());
     }
 
     let password = PasswordQuery::new("Insert Vault master password").read();
     let pw_hashed = hasher::hash(&password, "aot/I3YepRSH5AaZe+oDEQ").unwrap(); //TODO: make salt configurable
-    
+
     let vault = match Vault::default(&pw_hashed) {
-        Ok(acc) => {
-            acc
-        },
+        Ok(acc) => acc,
         Err(cocoon::Error::Cryptography) => {
             return Err("Invalid vault master password!".to_string());
-        },
+        }
         _ => {
             return Err("Unknown error adding an entry.".to_string());
         }
--- src/utils/hasher.rs
+++ src/utils/hasher.rs
@@ -1,18 +1,16 @@
 use pbkdf2::{
-    password_hash::{
-        PasswordHash, PasswordHasher, PasswordVerifier
-    },
-    Pbkdf2
+    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier},
+    Pbkdf2,
 };
 
-pub fn hash(password: &str, salt: &str) -> Result<String, &'static str> { 
+pub fn hash(password: &str, salt: &str) -> Result<String, &'static str> {
     let pwd = password.as_bytes();
     // let salt = SaltString::generate(&mut OsRng);
     // let salt = salt.as_bytes();
     // println!("salt {}", &salt.as_str());
     // Hash pwd to PHC string ($pbkdf2-sha256$...)
     let password_hash = Pbkdf2.hash_password(pwd, &salt).unwrap().to_string();
-    
+
     // Verify pwd against PHC string
     let parsed_hash = PasswordHash::new(&password_hash).unwrap();
     assert!(Pbkdf2.verify_password(pwd, &parsed_hash).is_ok());
