4316df1c04240c4e1295fe0cc0fc7e1e85b236ab
Andrew O'Doherty <andrew.olv@gmail.com>
2022-02-07 19:30:49 +0100

    feat: one time password authenticator

--- src/bin/cli_client/main.rs
+++ src/bin/cli_client/main.rs
@@ -1,30 +1,30 @@
 pub mod generator;
-pub mod vault_cli;
 pub mod options;
+pub mod vault_cli;
 
-pub use generator::{RandomPassword, random_password};
-pub use vault_cli::{add_entry};
-pub use options::{options, Opt, PasswordGenOpt, FlagsOpt, ManagerOpt, AddSiteOpt};
+pub use generator::{random_password, RandomPassword};
+pub use options::{options, AddSiteOpt, FlagsOpt, ManagerOpt, Opt, PasswordGenOpt};
 use std::process;
+pub use vault_cli::add_entry;
+
+use salty::utils::authenticator::Authenticator;
 
 fn main() {
     let opt = options::options();
-    
     let result: Result<(), String> = match opt {
         Opt::Generator(params) => {
-            let pass = generator::random_password(params).expect("Failed to generate random password");
+            let pass =
+                generator::random_password(params).expect("Failed to generate random password");
             println!("{}", pass);
             Ok(())
-        },
-        Opt::CreateVault => {
-            vault_cli::create_vault()
         }
-        Opt::AddSite(params) => {
-            vault_cli::add_entry(params)
-        },
-        Opt::ShowEntries => {
-            vault_cli::show_entries()
-        },
+        Opt::CreateVault => vault_cli::create_vault(),
+        Opt::AddSite(params) => vault_cli::add_entry(params),
+        Opt::ShowEntries => vault_cli::show_entries(),
+        Opt::Totp => {
+            Authenticator::new().validate_code();
+            Ok(())
+        }
     };
 
     if let Err(msg) = result {
